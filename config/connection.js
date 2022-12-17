704993ef9b4a64a38584b1fc5664923a2f2c2f7e
VectorCrop <73833509+vectorcrop@users.noreply.github.com>
2022-12-17 11:26:24 +0000

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,8 @@ const state = {
 
 module.exports.connect = function (done) {
   const url =
-    "mongodb+srv://msb:msb.com001@cluster0.vtkzb9y.mongodb.net/cluster0?retryWrites=true&w=majority";
+        "mongodb+srv://forskor:forskor.com001@forskor.vs3wn9x.mongodb.net/?retryWrites=true&w=majority";
+//     "mongodb+srv://msb:msb.com001@cluster0.vtkzb9y.mongodb.net/cluster0?retryWrites=true&w=majority";
   const dbname = "forskor";
 
   mongoClient.connect(url, { useUnifiedTopology: true }, (err, data) => {
